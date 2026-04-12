912803126bb2fdb4a4baf8938a6f3187d579dd3e
milajo2 <78371893+milajo2@users.noreply.github.com>
2026-04-12 15:19:43 +0000

    adjusted text

--- src/content/notifications.ts
+++ src/content/notifications.ts
@@ -12,16 +12,16 @@ The purpose of creating a feature that reminds users to respond back to calls an
 The users this would impact the most are people who lead a busy life who struggle with digital communication within this 24/7 digital age. This would people who are constantly working or people who aren’t as tied to their phone so do not respond to messages and calls in a respectable time.`,
 
   researchTitle: "Research",
-  surveyTitle: "User Survey",
-  surveyBody: `I created a survey that was sent out to phone users who used Android phones more specifically users who lead a busy lifestyle with a mixture of work, social and other my findings are as shown below:`, 
+  surveyTitle: "User surveys",
+  surveyBody: "I created a survey that was sent out to phone users who used Android phones more specifically users who lead a busy lifestyle with a mixture of work, social and other my findings are as shown below:",
   surveyStats: [
     "48% tend to forget to respond to calls and messages",
     "22% found that they respond a day or more later to the message/call",
-    "9% do not even respond back if the timeframe has been too long to call or message back ",
+    "9% do not even respond back if the timeframe has been too long to call or message back",
     "72% said they would benefit from a feature that could help with missed calls and messages"
   ],
-  userQuote: `Honestly I'm constantly on the move so I don't really check my calls and messages so I tend to forget the call or message I received at the time`,
-
+  userQuote: "Honestly I'm constantly on the move so I don't really check my calls and messages so I tend to forget the call or message I received at the time",
+  
   analysisTitle: "Competitive Analysis",
   analysisBody1: `I looked at different apps currently on the market, such as iOS notifications, Android notifications, and other third-party apps like Notification History Log. I found that while these apps allow you to see previous notifications, they do not allow you to prioritize them or set reminders for them.`,
 
